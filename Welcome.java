ad9fa4b4932d4221f36876ebf44f643a170d6826
yuchao86 <yuchao86@gmail.com>
2013-09-05 11:15:18 +0800

    first commit

---
+++ Welcome.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+public class Welcome{
+
+  public static void main(String[] args){
+    System.out.println("Welcome to java!!");
+    JOptionPane.showMessageDialog(null,"Welcome to Java");
+  }
+}
+
+
